fbfa9decdf850c6cc7baae75fbeac9b5db1904a7
Endorel <helene.francke@gmail.com>
2018-05-19 13:34:35 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,49 @@
+
+const express = require('express');
+const app = express();
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const productRoutes = require('./api/routes/products');
+const orderRoutes = require('./api/routes/orders');
+
+mongoose.connect('mongodb+srv://Endorel:M00ch1ck@nodeapiproject-xroey.mongodb.net/test');
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+
+//Hantera CORS
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Header',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+    if (req.method === 'OPTIONS') {
+        res.header('Access-Control-Allow-Methods', 'PUT, POST, PATCH, DELETE, GET');
+        return res.status(200).json({});
+        //Behöver inte hoppa in i route-funktionerna då detta är en request som alltid görs
+        //som en slags kontroll för att se om den får göra den 'riktiga' requesten
+    }
+    next();
+});
+
+//routes
+app.use('/products', productRoutes);
+app.use('/orders', orderRoutes);
+
+app.use((req, res, next) => {
+    const error = new Error('Not found');
+    error.status = 404;
+    next(error);
+});
+
+app.use((error, req, res, next) => {
+    res.status(error.status || 500);
+    res.json({
+        message: error.message
+    });
+});
+
+module.exports = app;
