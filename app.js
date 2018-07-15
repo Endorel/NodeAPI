4f0ad42ebb99b474e3ecaf5281960e8abe05c48f
Endorel <helene.francke@gmail.com>
2018-07-15 12:42:18 +0200

    added user auth

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const mongoose = require('mongoose');
 
 const productRoutes = require('./api/routes/products');
 const orderRoutes = require('./api/routes/orders');
+const userRoutes = require('./api/routes/user');
 
 mongoose.connect('mongodb+srv://Endorel:' + process.env.MONGO_ATLAS_PW + '@nodeapiproject-xroey.mongodb.net/test');
 
@@ -32,6 +33,7 @@ app.use((req, res, next) => {
 //routes
 app.use('/products', productRoutes);
 app.use('/orders', orderRoutes);
+app.use('/user', userRoutes);
 
 app.use((req, res, next) => {
     const error = new Error('Not found');
